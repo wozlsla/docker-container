36f62209d65de04040ad503869c04225e63855ba
jimin <jiminseo0307@gmail.com>
2024-11-29 14:07:12 +0900

    feat: watch, graceful shutdown

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ const express = require('express');
 const app = express();
 
 
-app.listen(8080, () => {
+const server = app.listen(8080, () => {
     console.log('서버 실행중 http://localhost:8080');
 });
 
 
 app.get('/', (req, res) => {
-    res.send('안녕');
+    res.send('안녕하쇼 1128');
+})
+
+process.on('SIGTERM', () => {
+    server.close(() => {
+        console.log('HTTP server closed')
+    })
+})
+
+process.on('SIGINT', () => {
+    server.close(() => {
+        console.log('HTTP server closed')
+    })
 })
